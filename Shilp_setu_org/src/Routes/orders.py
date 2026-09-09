331594525dc3c2deb3676b816e4e15be317bcd86
Kunal Bhand <bhand.kunal02@gmail.com>
2026-09-09 17:42:50 +0530

    Add files via upload

---
+++ Shilp_setu_org/src/Routes/orders.py
@@ -0,0 +1,8 @@
+'''
+1)post/order
+
+2)get/orders/id
+
+3)put/orders/id
+
+'''
---
+++ Shilp_setu_org/src/Routes/product.py
@@ -0,0 +1,16 @@
+'''
+1)Post/Product 
+2)Get/Profile
+3)Get/Product{id}
+4)put/product
+5)Delete/product{id}
+
+
+
+
+
+'''
+
+
+
+
