a54dbdd25cf21a3126fc93a11b2c88299f38a108
Kunal <bhand.kunal02@gmail.com>
2026-09-09 21:43:36 +0530

    Routes added only endpoints but no Working ,
    Auth.py needs improvement

--- Shilp_setu_org/src/Routes/auth.py
+++ Shilp_setu_org/src/Routes/auth.py
@@ -1,14 +1,16 @@
 #----------Auth------
-#               1) Signup
-#               2)Login
+from src.app.Main import app
 
 
+# 1) Signup
+@app.post("/signup")
+def signup():
+    return {"Status":"SignUP done Sucessfully","DATA":" "}
 
 
-
-
-
-
-
+# 2)Login
+@app.post("/login")
+def signup():
+    return {"Status":"Login done Sucessfully","DATA":" "}
 
 
