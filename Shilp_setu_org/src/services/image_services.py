a19db6fcb86f48b69afbd02ea6404995ec2c7067
Shivam_Bharre <shivambharre100@gmail.com>
2026-09-10 06:44:09 +0530

    image upload to imagekit is working the image url in db is not working

--- Shilp_setu_org/src/services/image_services.py
+++ Shilp_setu_org/src/services/image_services.py
@@ -1,15 +1,24 @@
+import os
 
 from dotenv import load_dotenv
 from imagekitio import ImageKit
-import os
+
 load_dotenv()
 
 imagekit = ImageKit(
-    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
-    public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
-    url_endpoint=os.getenv("IMAGEKIT_URL"),
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY")
 )
 
+
 def upload_image(file, file_name):
-    result = imagekit.files.upload(file=file, file_name=file_name, folder="/media")
-    return {"url": result.url, "file_name": result.name, "file_id": result.file_id}
+    result = imagekit.files.upload(
+        file=file,
+        file_name=file_name,
+        folder="/media"
+    )
+
+    return {
+        "url": result.url,
+        "file_name": result.name,
+        "file_id": result.file_id,
+    }
